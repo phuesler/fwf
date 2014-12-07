e84d988d5970c10f74a1e922e951957d785c2a43
Guillermo Álvarez <guillermo@cientifico.net>
2014-12-07 17:36:09 +0100

    WIP

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,25 @@
 //= require_tree .
+
+var connection = new DataConnection();
+
+connection.onopen = function(e) { /* e.userid */ };
+connection.onmessage = function(message, userid) {};
+
+// share data with all connected users
+connection.send(file || data || 'text message');
+
+// share data between two unique users (i.e. direct messages)
+connection.channels['user-id'].send(file || data || 'text-message');
+
+// custom signaling gateway
+connection.openSignalingChannel = function(callback) {
+    return io.connect().on('message', callback);
+};
+
+// check existing connections
+connection.check('room-name');
+
+document.getElementById('setup-new-connection').onclick = function() {
+    connection.setup('room-name');
+};
+
